75126b839702f84c224be6c3a68feee5c7418828
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2024-06-24 20:36:48 -0300

    prueba

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-import app from './app.js';
+import express from 'express';
+import trabajosRoutes from './src/routes/index.js';
+import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
 import { sequelize } from './src/database/database.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const __dirname = process.cwd();
+const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: '*'
+}));
+
+
+app.use(express.json());
+
+
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+
+app.use('/uploads', express.static(uploadDir));
+
+
+app.use(trabajosRoutes);
+
 
 async function main() {
     try {
         await sequelize.sync({ force: false });
+
+
         app.listen(port, () => {
             console.log(`Server is listening on port ${port}`);
         });
@@ -17,4 +45,8 @@ async function main() {
     }
 }
 
+
 main();
+
+
+export default app;
